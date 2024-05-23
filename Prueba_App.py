26c4be9146ad07c5cb003decdec24eb640e60722
LuisGomez335 <98978488+LuisGomez335@users.noreply.github.com>
2024-05-23 14:24:18 -0500

    Prueba
    App script

---
+++ Prueba_App.py
@@ -0,0 +1,7 @@
+def procesar_mensaje(mensaje):
+    if mensaje == 'hola':
+        return 'Recibí hola'
+    elif mensaje == 'bien':
+        return 'Recibí bien'
+    else:
+        return 'Mensaje no reconocido'
